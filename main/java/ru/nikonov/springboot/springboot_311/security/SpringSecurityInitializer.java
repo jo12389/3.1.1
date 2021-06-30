4e8e6cb2b9ce550dfd79ef7796a1035f6d5a3baa
niksergei <75827926+niksergei@users.noreply.github.com>
2021-06-30 18:11:27 +0300

    Delete main directory

--- main/java/ru/nikonov/springboot/springboot_311/security/SpringSecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-/*
-package ru.nikonov.springboot.springboot_311.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
-}
-*/
